bac0e4b030836fcd8d62edbc9202adf1cec1e026
chrizLopez <yambachristianjee@gmail.com>
2019-09-21 15:14:13 +0800

    delete post added

--- blog/forms.py
+++ blog/forms.py
@@ -4,6 +4,11 @@ from .models import Post
 
 class PostForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
     class Meta:
         model = Post
         fields = ('title', 'text',)
